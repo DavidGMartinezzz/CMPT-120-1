69e493bddbab9ba1c188090344b773969634ae5f
David Martinez <112397936+DavidGMartinezzz@users.noreply.github.com>
2022-09-08 08:43:52 -0400

    njnjn
    I changed something.

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -2,20 +2,20 @@
 #Assignment 1: Debug.py
 #Fix any syntax errors you can spot and fulfill any other requests
 def main():
-    print(hello)
+    print("hello")
     print(3)
     print(10 + 10)
 
 
     #insert your name in this string below
-    print("My name is ")
+    print("My name is David")
     #this is printing a string and not an integer. how do we fix it?
     print("50")
     #make sure this one prints out the sum
     print("2 + 2")
 
     #insert your name into this variable
-    name = ""
+    name = "David"
     print("Hello, my name is ", name)
 
     #insert your best friend's name here and have it print out
