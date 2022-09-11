67ced67ee6146ecc6b11d02d32cb8bdc76dc3313
David Martinez <112397936+DavidGMartinezzz@users.noreply.github.com>
2022-09-11 03:19:39 -0400

    Revert "Update Debug.py"
    This reverts commit a304c2fd90e54c66195d5ace0b9c342f409874fd.

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,8 +1,13 @@
 #instructions: print out "Hello World!"
 #create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+print("Hello World!")
 
+color = "blue"
+print("My favorite color is: ", color)
 
+birthday = "1212"
+print("My birthday is: ", birthday)
 
 def main():
 
